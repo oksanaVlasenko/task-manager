2ce35fe3014aa22093f742fb1c207300a0b7ecfd
oksanaVlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2019-12-07 18:54:24 +0200

    Updates

---
+++ precache-manifest.cf6100e76561ce6df2422e69cf5f7b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29f0b73ed782d15aa6009c4156593e2",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "041ee7f929784798c9bf",
+    "url": "/task-manager/static/css/main.6333ddeb.chunk.css"
+  },
+  {
+    "revision": "c0d07279a04bbcd65c4a",
+    "url": "/task-manager/static/js/2.fda91103.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/task-manager/static/js/2.fda91103.chunk.js.LICENSE"
+  },
+  {
+    "revision": "041ee7f929784798c9bf",
+    "url": "/task-manager/static/js/main.91911452.chunk.js"
+  },
+  {
+    "revision": "f2587cfc7571fabd58e4",
+    "url": "/task-manager/static/js/runtime-main.b873ff2b.js"
+  }
+]);
